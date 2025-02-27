82d39fd14e75dff9a7a18962493ad4170fe25590
RajendraKumarKushwaha <rajendra.cs909@gmail.com>
2025-02-27 14:18:25 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json())
-app.use(cors());
+
+app.use(cors({
+    origin: 'https://online-store-olive-nine.vercel.app', // Allow your frontend
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization'
+  }));
+//app.use(cors());
 
 app.get("/",(req,res)=>{
     return res.status(200).send({message:"welcome to ecommerce"})
